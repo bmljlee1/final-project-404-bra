1fe6b5c68c7de1ee48c2a0bfb778897498c79da4
Charlotte Bell <lottie808@hotmail.co.uk>
2025-03-04 11:47:20 +0000

    pwa template init

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -5,8 +5,8 @@ import { createClient } from "@supabase/supabase-js";
 // Initialize Supabase with the project URL and API key from environment variables
 // Export the Supabase client for use in other files
 
-const SUPABASE_URL = import.meta.env.VITE_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env
-  .VITE_PUBLIC_SUPABASE_ANON_KEY as string;
+  .VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
